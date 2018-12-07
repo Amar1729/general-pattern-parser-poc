7e3a19cff63820d82c6e0882fd79f2966abf99ec
Amar1729 <amar.paul16@gmail.com>
2018-12-06 19:22:20 -0500

    cleaned Symbol and CFG construction (test this)

--- poc.py
+++ poc.py
@@ -29,6 +29,11 @@ class Symbol():
         else:
             self.str = "\\0"
 
+        # these are set beforehand in case input regex == None:
+        # in this case the symbol is just a placeholder, waiting to be updated
+        self.raw = None
+        self.regex = None
+
         if regex:
             self.raw = regex
             self.regex = re.compile(regex)
@@ -50,6 +55,8 @@ class Symbol():
 
     @staticmethod
     def create_parse_func(reg=None, function=None):
+        # TODO - possibly use param function to check against current function?
+        # possible approach to solving left recursion
         def func(inp_str):
             result = re.match(reg, inp_str)
             if result:
@@ -59,12 +66,6 @@ class Symbol():
                 return (inp_str[rem_len:], result if result else True)
             return (inp_str, False)
         return func
-
-    # i think for testing purposes I'll have to define a few more functions
-    # such as a set_line() ? maybe ?
-    # how else do i manually create a CFG (remember, for testing?)
-    def set_parse_func(self, f):
-        self.parse_func = f
 
     def parse(self, inp_str):
         return self.parse_func(inp_str)
@@ -77,7 +78,7 @@ class Symbol():
 
     def __add__(self, other):
         """
-        Change the parse function when symbols are added together
+        Update the parse function when symbols are added together (concatenation)
         """
 
         if not isinstance(other, Symbol):
@@ -107,6 +108,9 @@ class Symbol():
         return s
 
     def __or__(self, other):
+        """
+        Update the parse function when symbols are or'd together (disjunction)
+        """
 
         if not isinstance(other, Symbol):
             raise TypeError("cannot or with type: {}".format(type(other)))
@@ -199,38 +203,49 @@ class CFG():
         # by default, the first symbol will be the start point
         self.root = lines[0][:lines[0].index(':')].strip().lstrip('\\')
 
-        self.__create_mappings(lines)
-        self.__create_cfg()
+        self._create_cfg(lines)
 
-    def __create_mappings(self, lines):
+    def _create_cfg(self, lines):
         """
-        extract symbol names and expressions into a dictionary
+        First, extract symbol names and expressions into a dictionary
+        (first step has to complete so symbol dict is full before step 2)
+        Then, from mappings, create the collection of Symbols that form this CFG
         """
+
         def _sym(inp):
             return inp[:inp.index(':')].strip().lstrip('\\')
 
         def _expr(inp):
             return re.sub(r'.*?:', '', inp, 1)
 
-        self.mappings = {_sym(line):_expr(line) for line in lines}
-
-    def __create_cfg(self):
-        """
-        From mappings, create the collection of Symbols that form this CFG
-        """
+        self.symbols = {}
+        self.mappings = self._mappings = {}
+        # TODO - can this loop be condensed ?
+        for line in lines:
+            _symbol = _sym(line)
+            self.mappings[_symbol] = self._mappings[_symbol] = _expr(line)
+            self.symbols[_symbol] = Symbol(None)
 
         # idea:
         # convert each line to a list of symbols to be combined in one loop
         #   (so that every symbol resolves to something)
         # then loop through again to perform Symbol arithmetic
-        #self.symbols = {k:self.__expand_symbol(v) for k, v in self.mappings.items()}
-        self.symbols = {}
-        for k, v in self.mappings.items():
+        def _line_to_expr(sym, line):
+            # TODO - where is best place to handle errors?
+            tokenized_line = self.__line_to_postfix(line)
+            symbol_expanded = self.__condense(tokenized_line)
+            # uh something here doesn't update properly?
+            self.symbols[sym].update(symbol_expanded)
+
+        _ = [_line_to_expr(symbol, line) for symbol, line in self.mappings.items()]
+
+        """
+        for symbol_skel, symbol_line in self.mappings.items():
             # is this loop necc?
-            self.symbols[k] = Symbol(None)
-            _k = self.__expand_symbol(v)
-            self.symbols[k].set_parse_func(_k.parse_func)
-        #self.symbols = {k:symbols[k].condense() for k in symbols}
+            tokenized_line = self.__line_to_postfix(symbol_line)
+            symbol_expanded = self.__condense(tokenized_line)
+            self.symbols[symbol_skel].update(symbol_expanded)
+        """
 
     @staticmethod
     def __split(line):
@@ -261,14 +276,6 @@ class CFG():
 
     def __is_symbol(self, _str):
         return True if _str.strip().lstrip('\\') in self.mappings else False
-
-    def __expand_symbol(self, line):
-        """
-        Convert a line with Symbol names to a Symbol
-        """
-
-        # TODO - maybe put ParseError handling/raising here, for more modular code?
-        return self.__condense(CFG.__line_to_postfix(line))
 
     @staticmethod
     def __line_to_postfix(line):
@@ -319,13 +326,9 @@ class CFG():
         if '(' in stack:
             raise ParseError("Unmatched '('")
 
-        while stack:
-            output.append(stack.pop())
-
-        print("postfix:")
-        print(output)
-        print('')
-        return output
+        # benchmark this? is this efficient?
+        for elem in output+stack[::-1]:
+            yield elem
 
     def __condense(self, tokens):
         """
@@ -354,11 +357,11 @@ class CFG():
             else:
                 # token is a str literal, regex, or SYMBOL_NAME
                 sym_name = token.strip().lstrip('\\')
-                if sym_name in self.mappings:
+                if sym_name in self.symbols:
                     # TODO - this line right here needs some work
                     # how to properly do symbol expansion and formation?
                     # needs to rely on .parse_func being set() later on (i think?)
-                    stack.append(self.mappings[sym_name]) # note that this is NOT a literal symbol
+                    stack.append(self.symbols[sym_name]) # note that this is NOT a literal symbol
                 else:
                     # literal Symbols can be easily recombined later
                     stack.append(Symbol(token, type_='lit'))
